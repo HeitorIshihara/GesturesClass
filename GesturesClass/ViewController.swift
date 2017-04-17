c46e596b14a4996455e02b25f1d370242862ca20
Heitor Ishihara <ishihara.heitor@gmail.com>
2017-04-17 16:05:15 -0300

    Adicionada animacao de desacelaracao

--- GesturesClass/ViewController.swift
+++ GesturesClass/ViewController.swift
@@ -28,7 +28,34 @@ class ViewController: UIViewController {
                                   y:view.center.y + translation.y)
         }
         recognizer.setTranslation(CGPoint.init(x: 0, y: 0), in: self.view)
+        
+        if recognizer.state == UIGestureRecognizerState.ended {
+            // 1
+            let velocity = recognizer.velocity(in: self.view)
+            let magnitude = sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y))
+            let slideMultiplier = magnitude / 200
+            print("magnitude: \(magnitude), slideMultiplier: \(slideMultiplier)")
+            
+            // 2
+            let slideFactor = 0.1 * slideMultiplier     //Increase for more of a slide
+            // 3
+            var finalPoint = CGPoint(x:recognizer.view!.center.x + (velocity.x * slideFactor),
+                                     y:recognizer.view!.center.y + (velocity.y * slideFactor))
+            // 4s
+            finalPoint.x = min(max(finalPoint.x, 0), self.view.bounds.size.width)
+            finalPoint.y = min(max(finalPoint.y, 0), self.view.bounds.size.height)
+            
+            // 5
+            UIView.animate(withDuration: Double(slideFactor * 2),
+                           delay: 0,
+                           // 6
+                options: UIViewAnimationOptions.curveEaseOut,
+                animations: {recognizer.view!.center = finalPoint },
+                completion: nil)
+        }
     }
+    
+    
 }
 
 
